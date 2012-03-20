46d4e968c9c2480d87ebf1554f280cfcb4c19201
Andrew Dixon <dixon@atomicbroadcast.net>
2012-03-20 15:07:12 -0700

    Added Method to get Group Posts
    Added method in query_methods.rb to grab the posts from a given
    group. Linkedin limits the number of posts returned to 50 so it
    might be a good idea to add the logic to get all posts into this
    function, and return everything as 1 big mash. Also, there is no
    test coverage because I don't really know how vcr works yet.

--- lib/linked_in/api/query_methods.rb
+++ lib/linked_in/api/query_methods.rb
@@ -25,7 +25,8 @@ module LinkedIn
 
       def groups(options={})
         path = "#{person_path(options)}/group-memberships?membership-state=member"
-        simple_query(path, options)
+        group_mash = simple_query(path, options)
+        group_mash.all
       end
 
       def suggested_groups(options={})
@@ -33,6 +34,22 @@ module LinkedIn
         simple_query(path, options)
       end
 
+      #
+      # group_posts(options={})
+      #
+      # inputs: 
+      #   REQUIRED
+      #     :group_id - the numeric id of the group to pull posts from.
+      #   OPTIONAL
+      #     :count - number of posts to retun (max 50)
+      #     :start - post number to start pulling from
+      #
+      # outputs:
+      #   Mash of json results from LinkedIn service
+      #
+      # description: Retreive posts from a given group and filter
+      #              based on optional inputs.
+      #
       def group_posts(options={})
         options.reverse_merge!({:order => "recency"})
         if options[:group_id]
@@ -40,7 +57,7 @@ module LinkedIn
         else
           raise ":group_id option required for group_posts method"
         end
-        path = "/groups/#{group_id}/posts:(title,summary,creator)"
+        path = "/groups/#{group_id}/posts:(title,summary,creator,id)"
         simple_query(path, options)
       end
 
